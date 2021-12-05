5de34714d5b2d635364741a67ee09c9c68af9f16
Elias Filho <eliasfilho202@gmail.com>
2021-12-04 22:44:48 -0300

    ativdade09

---
+++ atividades/atividade09/hosts.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+action=0
+host="vazio"
+proc="vazio"
+ip="vazio"
+
+
+
+adicionar(){
+
+	echo "$1 $2" | tee -a hosts.db
+
+
+}
+
+
+remover(){
+
+	
+	sed -i "/$1/d" hosts.db
+}
+
+listar(){
+
+	cat hosts.db
+
+}
+
+procurar(){
+
+	grep "${1}" hosts.db
+
+}
+
+
+
+while getopts "a:i:dl" OPTVAR
+do
+	if [ "$OPTVAR" == "a" ]
+	then
+		host=$OPTARG
+		action=1
+	fi
+	
+	if [ "$OPTVAR" == "i" ]
+	then
+		ip=$OPTARG
+	fi
+	
+	
+	if [ "$OPTVAR" == "d" ]
+	then
+		host=$OPTARG
+		action=2
+	fi
+	
+	if [ "$OPTVAR" == "l" ]
+	then
+		action=3
+	fi
+done
+
+
+case $action in
+	"1")
+		adicionar $host $ip
+	;;
+	"2")
+		remover $host
+	;;
+	"3")
+		listar
+	;;
+		
+	"0")
+		procurar $host
+	;;
+	*)
+		echo "Deu ruim na escolha ai camarada"
+	;;
+
+
+
+esac
+
+
+
+
